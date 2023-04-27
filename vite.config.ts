8f2a8c731cbec6cffe6b65c30365ee304136d732
rae-el <rachelizawhite@gmail.com>
2023-04-27 14:45:59 +0800

    Work on PWA requirements and serviceWorker

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
+import {VitePWA, VitePWAOptions } from "vite-plugin-pwa"
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ['tasks.png', 'tasks_maskable.png'],
+  manifest:{"short_name": "Task It",
+    "name": "Task It",
+    "icons": [{
+      "src":"tasks.png",
+      "type":"image/png",
+      "sizes":"512x512"
+    },
+    {
+      "src":"tasks_maskable.png",
+      "type":"image/png",
+      "sizes":"512x512",
+      "purpose":"any maskable"
+    }
+    ],
+    "scope":"/",
+    "start_url": "/",
+    "display": "standalone",
+    "theme_color": "#000000",
+    "background_color": "#ffffff"
+}}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{port:5173},
-  plugins: [react(), viteCompression()],
+  plugins: [react(), viteCompression(), VitePWA(manifestForPlugin)],
 })
